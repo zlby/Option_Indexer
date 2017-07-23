f6b0295b75da849109be3ead44f6959ad426ca74
zlby <zlby970129@163.com>
2017-07-24 02:49:15 +0800

    多核问题test02 1708-2900 count

--- algorithm/interval/init_interval.py
+++ algorithm/interval/init_interval.py
@@ -47,7 +47,8 @@ def delete_none():
 def update_interval():
     truncate_interval()
     initialize_interval()
-    combinations = Intervals.objects.filter(Q(positive_option__code__startswith='m1709-c-2500'))
+    combinations = Intervals.objects.filter(Q(positive_option__code__startswith='m1708-c-2900'))
+    print(len(combinations))
     graph_builders = []
 
     for combination in combinations:
