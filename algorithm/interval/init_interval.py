e26d2d4f9907365f8dcd5b5c09f43383a47f27d0
Freyr_Wings <freyrwings@gmail.com>
2017-07-23 10:17:01 +0800

    use multiprocess to insert data

--- algorithm/interval/init_interval.py
+++ algorithm/interval/init_interval.py
@@ -1,7 +1,7 @@
 import itertools
 from algorithm.interval.graph_build import *
 from algorithm.data_provider.data_provider_django import *
-from multiprocessing import Process, Lock
+from multiprocessing import Process
 
 
 def initialize_interval():
@@ -38,47 +38,37 @@ def process_update(part_graph_builders):
             interval_obj.save()
 
 
-def process_combination(lock, combinations, graph_builders, dp):
-    process_graph_builders = []
-    for combination in combinations:
-        graph_builder = GraphBuilder(dp)
-        graph_builder.prepare(combination.positive_option,
-                              combination.negative_option,
-                              2000)
-        process_graph_builders.append(graph_builder)
-    lock.acquire()
-    try:
-        graph_builders.extends(process_graph_builders)
-    finally:
-        lock.release()
-
-
 def update_interval():
     truncate_interval()
     initialize_interval()
     combinations = Intervals.objects.all()
     graph_builders = []
-    dp = DjangoDataProvider()
 
-    lock = Lock()
-    core_num = 8
-    process_len = int(len(combinations) / core_num)
-    remain_len = len(combinations) % core_num
-
-    mark = 0
-    for i in range(8):
-        if i < remain_len:
-            extra = 1
-        else:
-            extra = 0
-        p = Process(target=process_combination, args=(lock,
-                                                      combinations[mark:mark+process_len+extra],
-                                                      graph_builders,
-                                                      dp))
-        print(i)
-        p.start()
+    for combination in combinations:
+        dp = DjangoDataProvider()
+        print(combination)
+        graph_builder = GraphBuilder(dp)
+        graph_builder.prepare(combination.positive_option,
+                              combination.negative_option,
+                              2000)
+        graph_builders.append(graph_builder)
 
     process_update(graph_builders)
+
+    # core_num = 8
+    # process_len = int(len(graph_builders) / core_num)
+    # remain_len = len(graph_builders) % core_num
+    #
+    # mark = 0
+    # for i in range(8):
+    #     if i < remain_len:
+    #         extra = 1
+    #     else:
+    #         extra = 0
+    #     p = Process(target=process_update, args=(graph_builders[mark:mark+process_len+extra]))
+    #     print(i)
+    #     p.start()
+
 
 # 子进程要执行的代码
 # def run_proc(name):
