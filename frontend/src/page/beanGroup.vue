094bc8fdd69337824f5e6ef29ca90e4080fbf3b8
iss2015302580235 <1094290934@qq.com>
2017-08-18 21:55:55 +0800

    损益图界面左侧

--- frontend/src/page/beanGroup.vue
+++ frontend/src/page/beanGroup.vue
@@ -1,32 +1,28 @@
 <template>
-	<div class="homepage fillcontain">
+	<div>
 		<head-second></head-second>
 		<div class="beangroup">
-			<el-col :span="8" :offset="1">
+			<el-col :span="9" :offset="1">
 				<div class="input">
-					<div class="partOne">
-						<el-card style="height:100px;margin-top:20px">
+					<div class="partOne" style="margin-top:20px">
+						<el-card style="height:60px;padding-top:20px;padding-left:15px">
 							<el-row :gutter="20">
-								<el-col :span="10">
-									<div style="margin-top:8px">
-										<el-col :span="4" style="vertical-align:center">
-										时间
-										</el-col>
-										<el-col :span="20">
-											<el-select v-model="value" placeholder="请选择时间">
+								<el-col :span="12">
+									<div style="">
+										<span style="vertical-align:center; margin-top:8px">
+											时间
+										</span>
+											<el-select v-model="value" placeholder="请选择时间" style="width:75%">
 												<el-option>123</el-option>
 											</el-select>
-										</el-col>
 									</div>
 								</el-col>
-								<el-col :span="14">
-									<div style="margin-top:8px">
-										<el-col :span="4" style="vertical-align:center">
-											现货量(吨)
-										</el-col>
-										<el-col :span="20">
-											<el-input placeholder="请输入现货量(吨)"></el-input>
-										</el-col>
+								<el-col :span="12">
+									<div style="">
+										<span style="vertical-align:center; margin-top:8px">
+											现货
+										</span>
+										<el-input placeholder="请输入现货量(吨)" style="width:75%"></el-input>
 									</div>
 								</el-col>
 							</el-row>
@@ -36,65 +32,164 @@
 					<div class="partTwo">
 						<el-row :gutter="20">
 							<el-col :span="12">
-								<el-card>
-									1
-								</el-card>
+								<div class="container">
+									<div class="wrapper" v-for="comboFuture in comboFutures">
+										<div class="kind">
+											<span>期货品种</span>
+											<el-select v-model="comboFuture.future" class="sel">
+												<el-option
+												v-for="item in futures"
+												:key="item"
+												:label="item"
+												:value="item"/>
+											</el-select>
+										</div>
+
+										<span>期货持仓</span>
+										<el-input class="inp" v-model="comboFuture.futureHold"/>
+
+									</div>
+									<el-button type="danger" @click="addFuture" style="float: right; margin-right: 20px; "><i class="el-icon-plus"></i></el-button>
+								</div>
 							</el-col>
 
 							<el-col :span="12">
-								<el-card>
-									2
-								</el-card>
+								<div class="container">
+									<div class="wrapper" v-for="comboOption in comboOptions">
+										<div class="kind">
+										<span>期权品种</span>
+											<el-select v-model="comboOption.option" class="sel">
+												<el-option
+												v-for="item in options"
+												:key="item"
+												:label="item"
+												:value="item"/>
+											</el-select>
+										</div>
+
+										<span>期权持仓</span>
+										<el-input class="inp" v-model="comboOption.optionHold"/>
+
+										<div class="kind">
+										<span style="margin-left:15px">波动率</span>
+											<el-select v-model="comboOption.ratio" class="sel">
+												<el-option
+												v-for="item in ratios"
+												:key="item"
+												:label="item"
+												:value="item"/>
+											</el-select>
+										</div>
+
+									</div>
+									<el-button type="danger" @click="addOption" style="float: right; margin-right: 20px; "><i class="el-icon-plus"></i></el-button>
+								</div>
 							</el-col>
 						</el-row>
 					</div>
 
 					<div class="partThree">
-						<el-button class="el-col el-col-xs-24 el-col-md-24 el-col-sm-24 el-col-lg-24" style="background-color: #FEE090;color: #FEE090;">
-						<div style="color: #656565;font-size: 24px">开&nbsp&nbsp&nbsp始</div></el-button>
+						<el-button class="el-col el-col-xs-24 el-col-md-24 el-col-sm-24 el-col-lg-24" style="background-color: #FEE090;color: #314057;border: 4px solid #F9D481;">
+							<div style="color: #656565;font-size: 24px">开&nbsp&nbsp&nbsp始</div></el-button>
+						</div>
 					</div>
-				</div>
-			</el-col>
-			<el-col :span="15">
-				<div class="graph">
-				</div>
-			</el-col>
+				</el-col>
+				<el-col :span="14">
+					<div class="graph">
+					</div>
+				</el-col>
+			</div>
 		</div>
-	</div>
-</template>
+	</template>
 
-<script>
-	import headSecond from '../components/headSecond'
-	import footerBottom from '../components/footerBottom'
-	export default{
+	<script>
+		import headSecond from '../components/headSecond'
+		import footerBottom from '../components/footerBottom'
+		export default{
 
-		components:{
-			headSecond
-		},
+			components:{
+				headSecond,
+			},
 
-		data(){
-			return{
+			data(){
+				return{
+					futures: ['A', 'B', 'C', 'D'],
+					comboFutures: [],
+
+					options: ['1','2','3'],
+					ratios: ['1','2','3'],
+					comboOptions: [],
+				}
+			},
+			methods:{
+				addFuture: function() {
+					this.comboFutures.push({'future': null, 'futureHold': 0})
+				},
+				addOption: function() {
+					this.comboOptions.push({'option': null, 'ratio': null, 'optionHold': 0})
+				}
 			}
 		}
+
+	</script>
+
+	<style lang="less">
+		@import '../style/common';
+	
+
+		.el-input{
+			border-radius: 6px;
+			border: 3px solid #F9D481;
+			background:rgba(0, 0, 0, 0)
+		}
+
+		.partOne{
+			border-radius: 5px;
+			border: 2px solid #2C2E3B;
+		}
+
+
+		.partTwo{
+			margin-top: 20px;
+		}
+
+		.partThree{
+			margin-top: 20px;
+		}
+
+		.el-button{
+		}
+
+
+		.container {
+			padding-top: 10px;
+			padding-bottom: 10px;
+			padding-left: 12px;
+		  width: 100%;
+		height: 500px;
+		border-radius: 5px;
+		border: 2px solid #2C2E3B;
+		overflow: auto;
+		background-color: #e4e4e4;
 	}
-
-</script>
-
-<style lang="less">
-	@import '../style/common';
-
-	.partOne{
-
+	.wrapper {
+		 width: 90%;
+		padding: 5px;
+		border-radius: 5px;
+		border: 1px solid #bababd;
+		background-color: white;
+		margin-bottom: 10px;
 	}
-
-	.partTwo{
-		margin-top: 20px;
+	.kind {
+		margin-top: 5px;
 	}
-
-	.partThree{
-		margin-top: 20px;
+	.sel {
+		margin-left: 18px;
+		 width: 50%;
 	}
-
-	.el-button{
+	.inp {
+		margin-left: 18px;
+		 width: 50%;
+		 margin-top: 5px;
 	}
 </style>
