1f3395249e0cebcf2b54e6d21b67459ab0c74eba
Alan <alan1995wang@outlook.com>
2017-08-20 20:42:26 +0800

    add c version binomial amer

--- algorithm/data_analyzer/sim_spot_futures.pyx
+++ algorithm/data_analyzer/sim_spot_futures.pyx
@@ -6,14 +6,10 @@ R"""Simulate the spot-futures prices function,
 from libc cimport math
 
 # annual compound interest
-r = math.log(1.03)
+cpdef float r = math.log(1.03)
 
 cdef float _future_price(float spot_price, float time_lag, float convenience_yield, float storage_costs_discounted_value):
-    cdef float u = storage_costs_discounted_value
-    cdef float y = convenience_yield
-    cdef float t = time_lag
-    cdef float s = spot_price
-    return (s + u) * math.exp(t * (r - y))
+    return (spot_price + storage_costs_discounted_value) * math.exp(time_lag * (r - convenience_yield))
 
 def future_price(s, t, y, u):
     return _future_price(s,t,y,u)
