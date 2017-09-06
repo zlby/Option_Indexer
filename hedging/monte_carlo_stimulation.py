316a928cb0dbfdd80bab0a883df017b393d593fa
zlby <zlby970129@163.com>
2017-09-06 14:11:12 +0800

    choose_combo

--- hedging/monte_carlo_stimulation.py
+++ hedging/monte_carlo_stimulation.py
@@ -1,5 +1,10 @@
 from option.models import *
 import itertools
+import random
+import time
+
+def get_hedge_cost(future_list, option_list):
+    pass
 
 def monte_carlo(future_list, option_list, physicals, w1, w2, time_future, time_now = None, max_cost = 50000, fmax = 50, omax = 50):
     if time_now == None:
@@ -25,8 +30,10 @@ def monte_carlo(future_list, option_list, physicals, w1, w2, time_future, time_n
     total_asset_tv0 = 10 * (spot_price_now * physicals) + 10 * total_future_tv0 + 10 * total_option_tv0
 
 
+
 def choose_combos(max_cost=50000, fmax=50, omax=50):
 
+    time1 = time.clock()
     query_set_future = Future.objects.all()
     query_set_option = Option.objects.all()
     fo_list_all = []
@@ -39,9 +46,10 @@ def choose_combos(max_cost=50000, fmax=50, omax=50):
         fo_list_all.append(future.code)
     for option in query_set_option:
         fo_list_all.append(option.code)
-    for i in range(1, 4):
+    for i in range(1, 3):
         iter = itertools.combinations(fo_list_all, i)
         fo_list_combo.append(list(iter))
+
 
     for combo_list in fo_list_combo:
         for combo in combo_list:
@@ -55,3 +63,27 @@ def choose_combos(max_cost=50000, fmax=50, omax=50):
                     dict_combo['future_list'].append(dict_code)
                 else:
                     dict_combo['option_list'].append(dict_code)
+                for _ in range(10):
+                    new_combo = {}
+                    new_combo['future_list'] = []
+                    new_combo['option_list'] = []
+                    for future_dict in dict_combo['future_list']:
+                        fu = {}
+                        fu['code'] = future_dict['code']
+                        fu['amount'] = random.randint(1, fmax)
+                        new_combo['future_list'].append(fu)
+                    for option_dict in dict_combo['option_list']:
+                        op = {}
+                        op['code'] = option_dict['code']
+                        op['amount'] = random.randint(1, omax)
+                        new_combo['option_list'].append(op)
+                    combo_all.append(new_combo)
+
+    for combo in combo_all:
+        hedge_cost = get_hedge_cost(combo['future_list'], combo['option_list'])
+        if 0 < hedge_cost < max_cost:
+            combo_chosen.append(combo)
+
+    return combo_chosen
+
+
