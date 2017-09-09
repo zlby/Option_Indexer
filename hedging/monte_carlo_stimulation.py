e13a0796acd2459687b8b103cc45ef1a8b1d04c5
zlby <zlby970129@163.com>
2017-09-10 00:09:08 +0800

    套保算法完成

--- hedging/monte_carlo_stimulation.py
+++ hedging/monte_carlo_stimulation.py
@@ -2,15 +2,18 @@ from option.models import *
 import itertools
 import random
 import datetime
+import time
 from algorithm.prediction.data_analyzer.regress_model import *
 import numpy as np
 from gain_loss.option_price_cal import get_option_price
 import algorithm.prediction.data_analyzer.calculate_hedge_cost as chc
+from gain_loss.history_vol import history_vol
 
 
 
-def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, time_future:datetime.datetime, dist, time_now = None, max_cost:float = 50000, fmax:int = 50, omax:int = 50):
-    if time_now == None:
+def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, time_future:datetime.datetime, dist, time_now = None, max_cost = 50000., fmax:int = 50, omax:int = 50):
+    time_bg = time.clock()
+    if time_now is None:
         time_now_with_seconds = datetime.datetime.now()
         time_now = datetime.datetime(year=time_now_with_seconds.year, month=time_now_with_seconds.month, day=time_now_with_seconds.day
                                  , hour=time_now_with_seconds.hour, minute=time_now_with_seconds.minute)
@@ -48,11 +51,25 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
     total_asset_tv0 = 10 * (spot_price_now * physicals) + 10 * total_future_tv0 + 10 * total_option_tv0
 
 
-    combo_list = choose_combos(max_cost, fmax, omax)
+    combo_list = choose_combos(time_future, max_cost, fmax, omax)
 
     for combo in combo_list:
         combo['v_list'] = []
         combo['v0_minus_v_list'] = []
+
+        combo['future_together'] = []
+        combo['option_together'] = []
+
+        for future in combo['future_list']:
+            combo['future_together'].append(future)
+        for option in combo['option_list']:
+            combo['option_together'].append(option)
+
+
+        for future in future_list:
+            combo['future_together'].append(future)
+        for option in option_list:
+            combo['option_together'].append(option)
 
 
     futures_in_list = []
@@ -65,14 +82,7 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
         futures_in_list.append(dic)
 
     for combo_dict in combo_list:
-        # for future in combo_dict['future_list']:
-        #     flag = False
-        #     for item in futures_in_list:
-        #         if future['code'] == item['code']:
-        #             flag = True
-        #             break
-        #     if not flag:
-        #         futures_in_list.append(future)
+
         for option in combo_dict['option_list']:
             flag = False
             for item in options_in_list:
@@ -111,10 +121,10 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
     spot_price_list = []
 
     if dist['type'] == 'normal':
-        for _ in range(100):
+        for _ in range(20):
             spot_price_list.append(np.random.normal(dist['argv'][0], dist['argv'][1]))
     if dist['type'] == 'triangle':
-        for _ in range(100):
+        for _ in range(20):
             spot_price_list.append(np.random.triangular(dist['argv'][0], dist['argv'][1], dist['argv'][2]))
     if dist['type'] == 'uniform':
         for _ in range(20):
@@ -146,13 +156,7 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
             total_future_tv = 0
             total_option_tv = 0
 
-            combo['future_together'] = combo['future_list']
-            combo['option_together'] = combo['option_list']
 
-            for future in future_list:
-                combo['future_together'].append(future)
-            for option in option_list:
-                combo['option_together'].append(option)
 
             for future in combo['future_together']:
                 for item in futures_in_list:
@@ -176,7 +180,7 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
         combo['P2_add_value'] = 0
         for value in combo['v0_minus_v_list']:
             combo['P2_add_value'] += value
-        combo['P2_value'] = max(combo['P2_value'], 0)
+        combo['P2_value'] = max(combo['P2_add_value'], 0)
         P1_list.append(combo['hedge_cost'])
         P2_list.append(combo['P2_value'])
 
@@ -200,14 +204,19 @@ def monte_carlo(future_list, option_list, physicals:float, w1:float, w2:float, t
         if combo['score'] > result_combo['score']:
             result_combo = combo
 
-    print(result_combo['score'])
-    return result_combo
+    time_com = time.clock()
+
+    print(time_com - time_bg)
+
+    return result_combo['future_list'], result_combo['option_list'], result_combo['score']
 
 
-def choose_combos(max_cost:float=50000, fmax=50, omax=50):
 
-    query_set_future = Future.objects.all()
-    query_set_option = Option.objects.all()
+
+def choose_combos(time_future:datetime.datetime, max_cost:float=50000, fmax=50, omax=50):
+
+    query_set_future = Future.objects.filter(delivery_day__gt = time_future)
+
     fo_list_all = []
     fo_list_combo = []
 
@@ -216,8 +225,12 @@ def choose_combos(max_cost:float=50000, fmax=50, omax=50):
 
     for future in query_set_future:
         fo_list_all.append(future.code)
+
+    query_set_option = Option.objects.filter(asset__in=fo_list_all)
+
     for option in query_set_option:
         fo_list_all.append(option.code)
+
     for i in range(1, 3):
         iter = itertools.combinations(fo_list_all, i)
         fo_list_combo.append(list(iter))
@@ -231,11 +244,11 @@ def choose_combos(max_cost:float=50000, fmax=50, omax=50):
         dict_ocp['code'] = item.code
         dict_ocp['current_price'] = HourOptionTreadingData.objects.filter(option=dict_ocp['code']).order_by('-time')[0].close_price
         dict_ocp['deposit_today'] = Option.objects.get(code=dict_ocp['code']).deposit_today
+        dict_ocp['volatility'] = history_vol(dict_ocp['code'])
         option_code_price_deposit_list.append(dict_ocp)
 
     for item in query_f:
-        dict_fcp = {}
-        dict_fcp['code'] = item.code
+        dict_fcp = {'code': item.code}
         dict_fcp['current_price'] = FutureTreadingData.objects.filter(future=dict_fcp['code']).order_by('-time')[0].close_price
         dict_fcp['deposit_today'] = Future.objects.get(code=dict_fcp['code']).deposit_today
         future_code_price_deposit_list.append(dict_fcp)
@@ -261,7 +274,7 @@ def choose_combos(max_cost:float=50000, fmax=50, omax=50):
                 for future_dict in dict_combo['future_list']:
                     fu = {}
                     fu['code'] = future_dict['code']
-                    fu['amount'] = random.randint(1, fmax)
+                    fu['amount'] = random.randint(-fmax, fmax)
                     for item in future_code_price_deposit_list:
                         if fu['code'] == item['code']:
                             fu['price'] = item['current_price']
@@ -271,25 +284,71 @@ def choose_combos(max_cost:float=50000, fmax=50, omax=50):
                 for option_dict in dict_combo['option_list']:
                     op = {}
                     op['code'] = option_dict['code']
-                    op['amount'] = random.randint(1, omax)
+                    op['amount'] = random.randint(-omax, omax)
                     for item in option_code_price_deposit_list:
                         if op['code'] == item['code']:
                             op['price'] = item['current_price']
                             op['deposit'] = item['deposit_today']
+                            op['volatility'] = item['volatility']
                             break
                     new_combo['option_list'].append(op)
                 combo_all.append(new_combo)
 
 
     for combo in combo_all:
-        # hedge_cost = chc.hedge_cost(combo['option_list'], [], [], [], combo['future_list'], [])
-        # print(hedge_cost)
-        # if 0 < hedge_cost < max_cost:
-        #     combo_chosen.append(combo)
-        hedge_cost = 1
-        combo['hedge_cost'] = hedge_cost
-    # return combo_chosen
-    return combo_all
+        dict_none = {'amount': 0, 'code': None, 'deposit': 0, 'price': 0}
+        option_list = combo['option_list']
+        future_list = combo['future_list']
+        c_l = []
+        c_s = []
+        p_l = []
+        p_s = []
+        f_l = []
+        f_s = []
+
+        for option in option_list:
+            if option['code'].find('p') == -1:
+                if option['amount'] >= 0:
+                    c_l.append(option)
+                else:
+                    new_op = option
+                    new_op['amount'] = abs(option['amount'])
+                    c_s.append(new_op)
+            else:
+                if option['amount'] >= 0:
+                    p_l.append(option)
+                else:
+                    new_op = option
+                    new_op['amount'] = abs(option['amount'])
+                    c_s.append(new_op)
+        for future in future_list:
+            if future['amount'] >= 0:
+                f_l.append(future)
+            else:
+                new_fu = future
+                new_fu['amount'] = abs(future['amount'])
+                f_s.append(new_fu)
+
+
+        if len(c_l) == 0:
+            c_l.append(dict_none)
+        if len(c_s) == 0:
+            c_s.append(dict_none)
+        if len(p_l) == 0:
+            p_l.append(dict_none)
+        if len(p_s) == 0:
+            p_s.append(dict_none)
+        if len(f_l) == 0:
+            f_l.append(dict_none)
+        if len(f_s) == 0:
+            f_s.append(dict_none)
+
+        combo['hedge_cost'] = chc.hedge_cost(c_l, c_s, p_l, p_s, f_l, f_s)
+        if 0. < combo['hedge_cost'] < max_cost:
+            combo_chosen.append(combo)
+
+
+    return combo_chosen
 
 
 
