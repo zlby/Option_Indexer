02015cbd438c71ebec6b688a2fa42ab81165d549
zlby <zlby970129@163.com>
2017-09-12 16:15:09 +0800

    change spider

--- data_importer/for_all_products_price.py
+++ data_importer/for_all_products_price.py
@@ -2,6 +2,7 @@
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from option.models import *
 
 
 def  get_page_number():
@@ -18,20 +19,28 @@ def get_one_page_data(pageNumber):
     table=soup.find('table',class_='ba')   #最后几个页面没有数据，暂时没有影响
 
     trs=table.find_all('tr')
+    crop_data_list = []
+
     for i in range(1,len(trs)):
         tds=trs[i].find_all('td')          #数据都在这
         product_name=tds[0].text.strip()
         middle_price=tds[1].text.strip()
-        change=tds[2].text.strip()
-        highest_price=tds[3].text.strip()
-        lowest_price=tds[4].text.strip()
-        trading_amount=tds[5].text.strip()
+        # change=tds[2].text.strip()
+        # highest_price=tds[3].text.strip()
+        # lowest_price=tds[4].text.strip()
+        # trading_amount=tds[5].text.strip()
         date=tds[7].text.strip()
-        print(product_name,middle_price,change,highest_price,lowest_price,trading_amount,date)
+        crop = Crop(time=date, type=product_name, price=(middle_price * 10000))
+        crop_data_list.append(crop)
+
+        # print(product_name,middle_price,change,highest_price,lowest_price,trading_amount,date)
     #print('This is page'+pageNumber)
+    Crop.objects.bulk_create(crop_data_list)
 def get_all_pages_data():
     for i in range(1,get_page_number()+1):
         page_number=str(i)
+        if i % 50 == 0:
+            print(i)
         get_one_page_data(page_number)
 def get_newest_date(): #获取最新日期，不一定是当前日期的前一天
     res = requests.get('http://www.xibeiap.com/template/xibei/jghq.jsp?market_id=27&iStart=1')
@@ -51,6 +60,7 @@ def daily_update():
         soup = BeautifulSoup(res.text, 'html.parser')
         table = soup.find('table', class_='ba')
         trs = table.find_all('tr')
+        crop_data_list = []
         continue_page=True
         if continue_page:
             for i in range(1, len(trs)):
@@ -59,14 +69,17 @@ def daily_update():
                 if date == newest_date:
                     product_name = tds[0].text.strip()  # 最新的数据在这
                     middle_price = tds[1].text.strip()
-                    change = tds[2].text.strip()
-                    highest_price = tds[3].text.strip()
-                    lowest_price = tds[4].text.strip()
-                    trading_amount = tds[5].text.strip()
-                    print(product_name, middle_price, change, highest_price, lowest_price, trading_amount, date)
+                    crop = Crop(time=date, type=product_name, price=(middle_price * 10000))
+                    crop_data_list.append(crop)
+                    # change = tds[2].text.strip()
+                    # highest_price = tds[3].text.strip()
+                    # lowest_price = tds[4].text.strip()
+                    # trading_amount = tds[5].text.strip()
+                    # print(product_name, middle_price, change, highest_price, lowest_price, trading_amount, date)
                 else:
                     continue_page=False
                     break
+
 
 if __name__=="__main__":
     daily_update()
