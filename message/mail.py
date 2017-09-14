5d4eee604cf5032da9ef6fb482a37987e5bd4585
zlby <zlby970129@163.com>
2017-09-14 14:19:56 +0800

    跨品种完成

--- message/mail.py
+++ message/mail.py
@@ -17,17 +17,18 @@ class MailSender(object):
     AddressType = 1
     FromAlias = 'Blazer团队'
     Version = '2015-11-23'
-    text_base = '尊敬的客户%s您好！\n' \
+    text_message_base = '尊敬的客户%s您好！\n' \
                 '基于您的选择和市场行情的变动，给您建议如下：\n' \
                 '\t买入期权代码：%s \n' \
                 '\t卖出期权代码：%s \n' \
                 '\t买入\卖出手数比：%s \n' \
                 '\t套利区间：%s \n' \
                 '希望我们的建议为您带来收益。Blazer团队竭诚为您服务。 \n'
+    text_hedging_base = '尊敬的客户%s您好！'
 
     @staticmethod
     def send_message(email, name, buy_option, sell_option, rate, interval):
-        text = MailSender.text_base % (name, buy_option, sell_option, rate, interval)
+        text = MailSender.text_message_base % (name, buy_option, sell_option, rate, interval)
         params = {
             'Action': MailSender.Action,
             'AccountName': MailSender.AccountName,
@@ -67,5 +68,7 @@ class MailSender(object):
         h = hmac.new((MailSender.ACCESS_KRY_SECRET + '&').encode(), string_to_sign.encode(), hashlib.sha1)
         signature = base64.encodebytes(h.digest()).strip()
         return signature
+
+
 
 
