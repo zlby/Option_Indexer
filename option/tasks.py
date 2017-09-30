ba359bf33ab231990225675827828478dba479da
Freyr_Wings <freyrwings@gmail.com>
2017-09-30 23:00:58 +0800

    update celery

--- option/tasks.py
+++ option/tasks.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 from celery import task
 from data_importer.spider import Spider
 from data_importer.news_spider import NewsSpider
-
+from data_importer.for_all_products_price import daily_update
+from data_importer.for_caution_money import update_deposit
 
 @task
 def news_spider():
@@ -12,3 +13,13 @@ def news_spider():
 @task
 def option_spider():
     Spider.get_all_data()
+
+
+@task
+def all_products_price_spider():
+    daily_update()
+
+
+@task
+def caution_money_spider():
+    update_deposit()
