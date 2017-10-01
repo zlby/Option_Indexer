bb2b49d9ef3a28090ee309f955f364e6928f1628
issjava2015312580176 <1250328606@qq.com>
2017-10-01 09:59:54 +0800

    some fix

--- frontend/src/components/headTop.vue
+++ frontend/src/components/headTop.vue
@@ -37,10 +37,10 @@
           </span>
           <el-dropdown-menu slot="dropdown"  class="main-page-dropdown" style="background-color:#404040; border:0px;">
             <el-dropdown-item>
-            <el-button type="text" @click="window.location.href='/static/pdf/INDEXER.pdf'">用户手册</el-button>
+            <el-button type="text" @click="win.location.href='/static/pdf/INDEXER.pdf'">用户手册</el-button>
             </el-dropdown-item>
             <el-dropdown-item>
-              <el-button type="text" @click="window.open='/static/video/INDEXER.mkv'">演示视频</el-button>
+              <el-button type="text" @click="win.open='/static/video/INDEXER.mkv'">演示视频</el-button>
             </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
@@ -105,6 +105,7 @@
     return {
       no_readCount:0,
       dialogFormVisible: false,
+      win:window,
       form: {
               // username: '',
               old_password: '',
