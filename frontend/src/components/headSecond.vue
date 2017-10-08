e9e7731a5b11a2646414e40b19e449bcf6549ade
issjava2015312580176 <1250328606@qq.com>
2017-10-09 01:31:29 +0800

    去除个人主页

--- frontend/src/components/headSecond.vue
+++ frontend/src/components/headSecond.vue
@@ -3,8 +3,7 @@
 		<el-row >
 		<el-menu class="el-menu-demo3" mode="horizontal" router>
 		<el-col :span= "5" :xs="5" :md="5" :lg="5" :sm="5"style="background-color: #FFCC66; min-width:200px">
-			<el-menu-item index="/" class="el-col el-col-8 el-col-xs-8 el-col-md-8 el-col-sm-8 el-col-lg-8"><i class="el-icon-share"></i></el-menu-item>
-			<el-menu-item index="/homepageIndividual/list" class="el-col el-col-16 el-col-xs-16 el-col-md-16 el-col-sm-16 el-col-lg-16">个人主页</el-menu-item>
+			<el-menu-item index="/" class="el-col el-col-24 el-col-xs-24 el-col-md-24 el-col-sm-24 el-col-lg-24"></el-menu-item>
 		</el-col>
 		</el-menu>
 		</el-row>
